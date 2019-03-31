9c3ec05ebaf8ef31f22ac0767f037335ef893189
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-31 03:16:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,21 +2,16 @@ class Person
   def name=(persons_name)
     @name = persons_name
   end
-def name
-  @name
-end
-end
-
-beyonce = Person.new
-beyonce.name = "beyonce"
-
-def job=(persons_job)
+  def name
+    @name
+  end
+  def job=(persons_job)
     @job = persons_job
     def job
     @job
   end
 end
 
-
 beyonce = Person.new
+beyonce.name = "beyonce"
 beyonce.job = "Singer"
