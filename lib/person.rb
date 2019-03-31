43465186d287a228bd5983a5049a9c683ba69bcc
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-31 03:14:04 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+def name
+  @name
+end
+end
+
+beyonce = Person.new
+beyonce.name = "beyonce"
+
+def job=(persons_job)
+    @job = persons_job
+    def job
+    @job
+  end
+end
+
+
+beyonce = Person.new
+beyonce.job = "Singer"
